150dadbdba3eaf253fd18195f2a86eed07062c8a
Dan Chiarlone <danilochiarlone@gmail.com>
2018-11-15 22:15:04 -0800

    React Navigation Part 2

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,9 +1,9 @@
 import React, {Component} from 'react';
-import {DISHES} from '../shared/dishes';
+import Home from './HomeComponent'
 import Menu from './MenuComponent';
-import { View, Platform } from "react-native";
+import { View, Button } from "react-native";
 import DishDetail from './DishDetailComponent';
-import { createStackNavigator } from "react-navigation";
+import { createStackNavigator, createDrawerNavigator } from "react-navigation";
 
 const MenuNavigator = createStackNavigator({
     Menu: { screen: Menu},
@@ -14,20 +14,52 @@ const MenuNavigator = createStackNavigator({
         headerStyle: {
             backgroundColor: '#512DA8'
         },
-        headerTintColor: '#fff', // iconse
+        headerTintColor: '#fff', // icons
         headerTitleStyle: {
             color: '#fff'
         }
    } // applies to all screens. 
 });
 
+const HomeNavigator = createStackNavigator({
+    Home: { screen: Home},
+}, {
+   navigationOptions: {
+        headerStyle: {
+            backgroundColor: '#512DA8'
+        },
+        headerTintColor: '#fff', // icons
+        headerTitleStyle: {
+            color: '#fff'
+        }
+   } // applies to all screens. 
+});
+
+const MainNavigator = createDrawerNavigator({
+    Home: { 
+        screen: HomeNavigator,
+        navigationOptions: {
+            title: 'Home',
+            drawerLabel: 'Home'
+        } // We use HomeNavigator so we can have navigation options
+    },
+    Menu: { 
+        screen: MenuNavigator,
+        navigationOptions: {
+            title: 'Menu',
+            drawerLabel: 'Menu'
+        } // We use HomeNavigator so we can have navigation options
+    }, 
+})
+
 class Main extends Component 
 {
+
     render()
     {
         return(
             <View style={{flex: 1, paddingTop: Expo.Constants.statusBarHeight}}>
-                <MenuNavigator/>
+                <MainNavigator/>
             </View>
         );
     }
