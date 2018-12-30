d6b73f7d6d763f5dd40a1ed04ca5dff67842c8a2
Dan Chiarlone <danilochiarlone@gmail.com>
2018-12-30 20:30:26 -0200

    Secure Store

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -6,6 +6,7 @@ import About from './AboutComponent';
 import DishDetail from './DishDetailComponent';
 import Reservation from './ReservationComponent';
 import Favorites from "./FavoriteComponent";
+import Login from './LoginComponent';
 import { View, Image, StyleSheet, ScrollView, Text } from "react-native";
 import { createStackNavigator, createDrawerNavigator, DrawerItems, SafeAreaView } from "react-navigation";
 import { Icon } from 'react-native-elements';
@@ -148,6 +149,25 @@ const FavoritesNavigator = createStackNavigator({
   } // applies to all screens. 
 });
 
+
+const LoginNavigator = createStackNavigator({
+    Login: { screen: Login},
+}, {
+   navigationOptions: ( { navigation } ) => ({
+        headerStyle: {
+            backgroundColor: '#512DA8'
+        },
+        headerTintColor: '#fff', // icons
+        headerTitleStyle: {
+            color: '#fff'
+        },
+        headerLeft: <Icon name='menu' size={24}
+            color='white'
+            onPress={() => navigation.toggleDrawer()}
+        />
+   }) // applies to all screens. 
+});
+
 const CustomDrawerContentComponent = (props) => (
     <ScrollView>
       <SafeAreaView style={styles.container} forceInset={{ top: 'always', horizontal: 'never' }}>
@@ -165,6 +185,21 @@ const CustomDrawerContentComponent = (props) => (
   );
 
 const MainNavigator = createDrawerNavigator({
+    Login: { 
+        screen: LoginNavigator,
+        navigationOptions: {
+            title: 'Login',
+            drawerLabel: 'Login',
+            drawerIcon: ({tintColor}) => (
+                <Icon
+                    name='sign-in'
+                    type='font-awesome'
+                    size={22}
+                    color={tintColor}
+                    />
+            )
+        }
+    },
     Home: { 
         screen: HomeNavigator,
         navigationOptions: {
@@ -256,6 +291,7 @@ const MainNavigator = createDrawerNavigator({
         } // We use HomeNavigator so we can have navigation options
     }
 }, {
+    initialRouteName: 'Home',
     drawerBackgroundColor: '#D1C4E9',
     contentComponent: CustomDrawerContentComponent
 })
